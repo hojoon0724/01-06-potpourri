7a1a5f30a3846baf3d15a31ac17edbf3ca10e66a
Hojoon Kim <hojoon0724@gmail.com>
2023-11-18 10:33:33 -0800

    odd number count done

--- potpourri/solutions.js
+++ potpourri/solutions.js
@@ -1,4 +1,7 @@
 console.log("yo");
+// -----------------------------------------------------
+// setAlarm
+// -----------------------------------------------------
 
 function setAlarm(employed, vacationing) {
   if (employed === true && vacationing === false) {
@@ -14,3 +17,15 @@ setAlarm(true, true);
 setAlarm(false, false);
 setAlarm(false, true);
 setAlarm(true, false);
+
+// -----------------------------------------------------
+// oddNumberCount
+// -----------------------------------------------------
+
+function oddNumberCount(inputNumber) {
+  const answer = Math.floor(inputNumber / 2);
+  console.log(answer);
+}
+
+oddNumberCount(9);
+oddNumberCount(32);
